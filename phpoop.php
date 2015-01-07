f0a464372201457390bb85887db331fca63773cf
Moises A. Cuevas <moisescuevas46@yahoo.com>
2015-01-06 20:43:57 -0800

    Did the lecture

---
+++ phpoop.php
@@ -0,0 +1,59 @@
+<?php 
+class = Dog {
+$firstName = “Coko”;
+$lastName = “Cuevas”;
+$gender = “male”;
+$price = Priceless;
+
+function getName() {
+return “{$this­>firstName}” .
+“{$this­>lastName}”;
+}
+
+}
+$dog1 = new Dog();
+$dog1­>firstName = “Boba”;
+$dog1­>lastName = “Drink”;
+
+print “The dog’s name is {$dog1­>getName()}.”;
+?>
+
+<?php 
+class = music {
+$band = “Muse”;
+$people = “c,m,d”;
+$gender = “male”;
+$price = alot;
+
+function getName() {
+return “{$this­>band}” .
+“{$this­>people}”;
+}
+
+}
+$music1 = new Band();
+$music1­>band = “Muse”;
+$music1­>people = “C,M,D”;
+
+print “The band’s name is {$music1­>getName()}.”;
+?>
+
+<?php 
+class = guitar {
+$firstName = “Electric”;
+$lastName = “Guitar”;
+$gender = “inatimate”;
+$price = depends;
+
+function getName() {
+return “{$this­>firstName}” .
+“{$this­>lastName}”;
+}
+
+}
+$guitar1 = new guitar();
+$guitar1­>firstName = “Electric”;
+$guitar1­>lastName = “Guitarz”;
+
+print “The guitars’s name is {$guitar1­>getName()}.”;
+?>
