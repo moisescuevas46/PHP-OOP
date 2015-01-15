9e1dd9258e1f8ba0010cd3bd7209680abbbee3dc
Moises A. Cuevas <moisescuevas46@yahoo.com>
2015-01-14 17:24:59 -0800

    lecture 5 2 good examples 1 dummy example

--- lecture5.php
+++ lecture5.php
@@ -37,3 +37,93 @@
  $cat = new Cat("Cat","Mellow","Yellow","male",15,"true");
  print "Animal 1 is a " . $cat->getName();
  ?>
+
+
+
+ ----------
+
+
+
+ <?php 
+ class school{
+ 	public $Name;
+ 	public $Grade;
+	public $Public;
+	public $Teacher;
+	public $Student;
+
+	function __construct($Name, $Grade, $Public, $Teacher, $Student){
+		$this->Name = $Name;
+		$this->Grade=$Grade ;
+		$this->Public= $Public;
+		$this->Teacher= $Teacher;
+		$this->Student= $Student;
+	}
+
+	function getName(){
+		return "This is my " . $this->firstName . "and its grades are " . $this->lastName . " and it is a " . $this->public ." school.";
+	}
+ }	
+ class Teach extends school{
+ 	function __construct($Name,$Grade,$Public,$Teacher,$Student){
+ 		parent::__construct($Name,$Grade,$Public,$Teacher,$Student);
+ 		$this->name = $Name;
+ 	}
+ }
+
+ class Student extends school{
+ 	function __construct($Name,$Grade,$Public,$Teacher,$Student){
+ 		parent::__construct($Name,$Grade,$Public,$Teacher,$Student);
+ 		$this->asd = $asd;
+ 	}
+ 	function hello(){
+ 		return $this->asd;
+ 	}
+ }
+ $cat = new Cat("Cat","Mellow","Yellow","male",15,"true");
+ print "Animal 1 is a " . $cat->getName();
+ ?>
+
+
+ ======================
+
+
+ <?php 
+ class school{
+ 	public $Name;
+ 	public $Grade;
+	public $Public;
+	public $Teacher;
+	public $Student;
+
+	function __construct($Name, $Grade, $Public, $Teacher, $Student){
+		$this->Name = $Name;
+		$this->Grade=$Grade ;
+		$this->Public= $Public;
+		$this->Teacher= $Teacher;
+		$this->Student= $Student;
+	}
+
+	function getName(){
+		return "This is my " . $this->firstName . "and its grades are " . $this->lastName . " and it is a " . $this->public ." school.";
+	}
+ }	
+ class Teach extends school{
+ 	function __construct($Name,$Grade,$Public,$Teacher,$Student){
+ 		parent::__construct($Name,$Grade,$Public,$Teacher,$Student);
+ 		$this->name = $Name;
+ 	}
+ }
+
+ class Student extends school{
+ 	function __construct($Name,$Grade,$Public,$Teacher,$Student){
+ 		parent::__construct($Name,$Grade,$Public,$Teacher,$Student);
+ 		$this->asd = $asd;
+ 	}
+ 	function hello(){
+ 		return $this->asd;
+ 	}
+ }
+ $cat = new Cat("Cat","Mellow","Yellow","male",15,"true");
+ print "Animal 1 is a " . $cat->getName();
+ ?>
